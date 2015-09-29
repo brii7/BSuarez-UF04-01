2d499f2d3cf92cd1dc37ac5bcae739af78cfa84e
briihd <briihd@gmail.com>
2015-09-29 16:05:27 +0200

    Afegir numerospetits

---
+++ numerospetits.java
@@ -0,0 +1,18 @@
+package numerospetitssubv;
+import java.util.Arrays;
+import java.util.Scanner;
+public class NumerosPetitsSUBV {
+    
+    public static void main(String[] args) {
+      Scanner lector = new Scanner(System.in);
+      int [] num = new int[5];
+      
+      for (int i=0; i<num.length;i++){
+          System.out.println("Entra un numero");
+          num[i] = lector.nextInt();
+      }
+      Arrays.sort(num);
+      System.out.println("El número més petit és "+num[0]);
+    }
+    
+}
